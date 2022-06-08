fe602b5e7276c2eab522ba2f7bdfb85383dbca26
Yin Loong Chao <ychao@akamai.com>
2022-06-09 06:32:57 +0700

    Updating message to linode workshop

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Selamat Jun 5');
+  res.send('Linode Workshop Example');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
