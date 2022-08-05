c8f5acf2158dc65dc879691eb0ffd7ab836818ae
Yin Loong Chao <ychao@akamai.com>
2022-08-05 12:03:11 +0800

    Edited selamat again

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('selmata FROM AYANA JAKARTA!');
+  res.send('elmata FROM AYANA JAKARTA!');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
